c415fa4af012319ec7107b6d7e9d48a519f46a9e
Junyoung McArdle <juny.mcardle@gmail.com>
2019-04-02 17:23:58 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+class Song
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @@count += 1
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+  
+  def self.artist
+    @@artists
+  end
+
+  def self.genre
+    @@genres
+  end
+
+  def self.genre_count
+  end
+
+  def self.artist_count
+  end
+
+end

811c777171a7e49275b518de603f2732af3b4615
Junyoung McArdle <juny.mcardle@gmail.com>
2019-04-02 17:48:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   end
 
   def self.genre_count
-    genre_count = Hash.new
+    genre_count = {}
     @@genres.each do |genre|
       if genre_count.include?(genre)
         genre_count[genre] += 1
